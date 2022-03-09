48868024a89a103037b532d93606312372807ee7
Hannah Chi <cremi468@gmail.com>
2022-03-09 21:38:15 +0800

    Initial Commit

---
+++ gif record/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gif record
+//
+//  Created by Hsiao-Han Chi on 2022/3/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
